f3a31229582fd36ccf548735573f035288df9afa
Nick Ostrovsky <legatus357@gmail.com>
2023-09-04 22:01:51 -0700

    Add project files.

---
+++ Nck_Ostrovsky_03_06_01_Rev_A/functions.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "header.h"
+
+
+
+    double num1;
+    double num2;
+
+   
+    bool set_num1(double nn) {
+        if (nn != 0) { num1 = nn; return (true); }
+        else { return (false); }
+    }
+    bool set_num2(double mm) {
+        if (mm != 0) { num2 = mm; return (true); }
+        else { return (false); }
+    }
+    double get_num1() { return num1; }
+    double get_num2() { return num2; }
+    double add()
+    {
+        return (num2 + num1);
+    }
+    double multiply()
+    {
+        return (num1 * num2);
+    }
+    double substruct_1_2()
+    {
+        return (num1 - num2);
+    }
+    double substruct_2_1()
+    {
+        return (num2 - num1);
+    }
+    double divide_1_2()
+    {
+        return (num1 / num2);
+    }
+    double divide_2_1()
+    {
+        return (num2 / num1);
+    }
+
+
+   
